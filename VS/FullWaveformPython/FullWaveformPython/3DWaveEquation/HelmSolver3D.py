b93b7b06b4a1314d7d54b6137ab27b2d2e65438b
Puneet Saraswat <PSaraswat@slb.com>
2014-11-16 21:25:48 -0600

    Start1

---
+++ VS/FullWaveformPython/FullWaveformPython/3DWaveEquation/HelmSolver3D.py
@@ -0,0 +1,14 @@
+import numpy as np
+import scipy as sp
+class HelmSolver3D:
+    """This class solves for 3D helm equation in a sparsifying techinique"""
+
+    def HelmSolve(helm, f, m, b, o , d, n , nb, beta, l):
+        N= 0
+        w=0.0
+        N= n[0]*n[1]*n[2]
+        w= 2*np.pi*f
+        p1 = np.array([1- np.power((beta/w)*1j* np.linspace(1,0, nb[0]),l), np.ones(n[0]-2*nb[0])], 1-np.power((beta/w)*1j*np.linspace(0,1, nb[0]),l))
+        p2 = np.array([1- np.power((beta/w)*1j* np.linspace(1,0, nb[1]),l), np.ones(n[1]-2*nb[1])], 1-np.power((beta/w)*1j*np.linspace(0,1, nb[1]),l))
+        p1 = np.array([1- np.power((beta/w)*1j* np.linspace(1,0, nb[2]),l), np.ones(n[2]-2*nb[2])], 1-np.power((beta/w)*1j*np.linspace(0,1, nb[2]),l))
+        return 
---
+++ VS/FullWaveformPython/FullWaveformPython/FullWaveformPython_test.py
@@ -0,0 +1,4 @@
+import HelmSolver3D as h3d
+
+obj1= h3d.HelmSolver3D()
+obj1.HelmSolve(
