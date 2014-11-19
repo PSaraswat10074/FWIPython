32134ecf3797084fa4311af39348c3e8cc276727
Puneet Saraswat <PSaraswat@slb.com>
2014-11-19 00:06:59 -0600

    Day1- forward

--- VS/FullWaveformPython/FullWaveformPython/FullWaveformPython_test.py
+++ VS/FullWaveformPython/FullWaveformPython/FullWaveformPython_test.py
@@ -1,4 +1,10 @@
 import HelmSolver3D as h3d
-
-obj1= h3d.HelmSolver3D()
-obj1.HelmSolve(
+import numpy as np1
+helm= h3d.HelmSolver3D()
+f=5
+m= np1.ones(46656)
+o=np1.array([-100 ,-100 ,-100])
+d=np1.array([20, 20, 20])
+n= np1.array([36, 36, 36])
+nb=np1.array([5,5,5])
+helm.HelmSolve( 5,m,m,o,d,n,nb,100,2)
