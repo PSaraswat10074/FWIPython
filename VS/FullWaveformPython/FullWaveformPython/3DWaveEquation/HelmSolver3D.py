ac21090943efbdf702c9c7e193716abd67765322
Puneet Saraswat <PSaraswat@slb.com>
2014-11-19 01:33:53 -0600

    Update on numpy matrices

--- VS/FullWaveformPython/FullWaveformPython/3DWaveEquation/HelmSolver3D.py
+++ VS/FullWaveformPython/FullWaveformPython/3DWaveEquation/HelmSolver3D.py
@@ -8,7 +8,7 @@ class HelmSolver3D:
         w=0.0
         N= n[0]*n[1]*n[2]
         w= 2*np.pi*f
-        p1 = np.array([1- np.power((beta/w)*1j* np.linspace(1,0, nb[0]),l), np.ones(n[0]-2*nb[0])], 1-np.power((beta/w)*1j*np.linspace(0,1, nb[0]),l))
-        p2 = np.array([1- np.power((beta/w)*1j* np.linspace(1,0, nb[1]),l), np.ones(n[1]-2*nb[1])], 1-np.power((beta/w)*1j*np.linspace(0,1, nb[1]),l))
-        p1 = np.array([1- np.power((beta/w)*1j* np.linspace(1,0, nb[2]),l), np.ones(n[2]-2*nb[2])], 1-np.power((beta/w)*1j*np.linspace(0,1, nb[2]),l))
+        p1 = np.array([[1- np.power((beta/w)*1j* np.linspace(1,0, nb[0]),l)], [np.ones(n[0]-2*nb[0])],[1-np.power((beta/w)*1j*np.linspace(0,1, nb[0]),l)]])
+        p2 = np.array([[1- np.power((beta/w)*1j* np.linspace(1,0, nb[1]),l)], [np.ones(n[1]-2*nb[1])], [1-np.power((beta/w)*1j*np.linspace(0,1, nb[1]),l)]])
+        p1 = np.array([[1- np.power((beta/w)*1j* np.linspace(1,0, nb[2]),l)], [np.ones(n[2]-2*nb[2])], [1-np.power((beta/w)*1j*np.linspace(0,1, nb[2]),l)]])
         return 
